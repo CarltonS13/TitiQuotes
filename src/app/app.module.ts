571c539cc304708194e8ad612a327f5c175b8e91
Carlton Segbefia <carltonsegbefia@Carltons-MacBook-Pro.local>
2017-10-08 16:48:37 -0500

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,47 @@
+import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+
+
+import { QuotePage } from '../pages/quote/quote';
+import { FavsPage } from '../pages/favs/favs';
+import { ContactPage } from '../pages/contact/contact';
+import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { SocialSharing } from '@ionic-native/social-sharing';
+import { LocalNotifications } from '@ionic-native/local-notifications';
+import { QuotesProvider } from '../providers/quotes/quotes';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
-    ListPage
+    QuotePage,
+    FavsPage,
+    ContactPage,
+    TabsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
+    IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage,
-    ListPage
+    QuotePage,
+    FavsPage,
+    ContactPage,
+    TabsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    SocialSharing,
+    LocalNotifications,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    QuotesProvider
   ]
 })
 export class AppModule {}
